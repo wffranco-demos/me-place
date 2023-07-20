a07e1b5bdf1a74b11ce9b0f6584e020d5d21e2d5
Willem Franco <willemfranco@gmail.com>
2023-07-20 16:12:00 -0500

    Fix darmode (but still hidden)

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -3,11 +3,15 @@ class DarkMode {
     const mode = localStorage.getItem('mode');
     return mode === 'dark' || (mode !== 'light' && window.matchMedia('(prefers-color-scheme: dark)').matches);
   }
+  static get mode() {
+    const mode = localStorage.getItem('mode');
+    return mode === 'dark' || mode === 'light' ? mode : 'pc';
+  }
   static toggle() {
     const mode = localStorage.getItem('mode');
     if (mode === 'dark') localStorage.setItem('mode', 'light');
     else if (mode === 'light') localStorage.removeItem('mode');
-    else localStorage.setItem('dark');
+    else localStorage.setItem('mode', 'dark');
     return DarkMode.enabled;
   }
 }
